bff010e827a33790c622262c33797832d6d70c75
Vince Oblak <voblak2@gmail.com>
2020-09-02 17:02:09 -0500

    like, a lot of else if's

--- Program.cs
+++ Program.cs
@@ -6,16 +6,22 @@ namespace lesson2
     {
         static void Main(string[] args)
         {
-            var a = 5;
-            var b = 6;
-            if (a == 6)
+            int score = 71;
+            string grade = "";
+
+            if (score > 90)
             {
-                Console.WriteLine("A equals six!");
-            } else if (b == 6) {
-                Console.WriteLine("B equals six!");
-            } else {
-                Console.WriteLine("False");
+                grade = "A";
+            } 
+            else if (score > 80) 
+            {
+                grade = "B";
+            } 
+            else if (score > 70) 
+            {
+                grade = "C";
             }
+            Console.WriteLine(grade);
         }
     }
 }
