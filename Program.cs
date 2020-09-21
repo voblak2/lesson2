b84e4628ebeda58056ab8a300a6015d303dff268
Vince Oblak <voblak2@gmail.com>
2020-09-21 10:37:37 -0500

    math ops

--- Program.cs
+++ Program.cs
@@ -6,15 +6,8 @@ namespace lesson2
     {
         static void Main(string[] args)
         {
-            var userAge = 22;
-
-            if (userAge < 18)
-            {
-                Console.WriteLine("Sorry, you are too young to purchase tickets for this movie.");
-            }   else if (userAge >= 18)
-            {
-                Console.WriteLine("Here are your tickets. Enjoy the movie.");
-            }
+            var quotient = 6 / 3;
+            Console.WriteLine(quotient);
         }
     }
 }
